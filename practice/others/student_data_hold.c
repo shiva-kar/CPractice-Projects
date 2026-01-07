71bed8b166cc1af180b3cf0a8cfbc410db0b498f
Quantum <sksmarttech48@gmail.com>
2026-01-08 02:26:36 +0530

    Refactor Fibonacci function and update student data logic
    Renamed the Fibonacci function to 'ways' in fibonacci_recursive.c for clarity and updated all references. Modified student_data_hold.c to use an infinite loop for input collection and removed the data.txt file. Updated corresponding executables.

--- practice/others/student_data_hold.c
+++ practice/others/student_data_hold.c
@@ -5,8 +5,9 @@ int main()
 	FILE *file = fopen("data.txt", "w");
 	char ch;
 
-	while (ch != '\n')
+	while (1)
 	{
+
 		printf("Enter student name:\t");
 		scanf("%c", &ch);
 		printf("Enter student marks:\t");
@@ -15,7 +16,7 @@ int main()
 		scanf("%c", &ch);
 		ch = fputc(ch, file);
 	}
-	
+
 	fclose(file);
 	return 0;
 }
--- practice/recursion/fibonacci_recursive.c
+++ practice/recursion/fibonacci_recursive.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int fib(int n);
+int ways(int n);
 
 int main(void){
 	int till;
@@ -8,14 +8,14 @@ int main(void){
 	if (scanf("%d", &till) != 1 || till < 0) return 0;
 
 	for (int i = 0; i <= till; i++) {
-		int result = fib(i);
+		int result = ways(i);
 		printf("%d%s", result, i == till ? "\n" : " ");
 	}
 	return 0;
 }
 
-int fib(int n){
+int ways(int n){
 	if (n == 0) return 0;
 	if (n == 1) return 1;
-	return fib(n - 1) + fib(n - 2);
+	return ways(n - 1) + ways(n - 2);
 }
